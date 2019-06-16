08ece77534b0a1b1ae031f1d4d7983f6be7e68c4
gerritt <gmocore@gmail.com>
2019-06-16 16:03:59 -0600

    added partially functional progress countdown bar

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const clickArea = document.querySelector('.click-area');
 const timer = document.getElementById('timer');
-const five = document.getElementById('5');
-const ten = document.getElementById('10');
-const eleven = document.getElementById('11');
-const twelve = document.getElementById('12');
+const thirty = document.getElementById('30');
+const sixty = document.getElementById('60');
+const ninety = document.getElementById('90');
+const oneTwenty = document.getElementById('120');
 const clickDisplay = document.getElementById('click-display');
+const timerCountdownProgressBar = document.querySelector(".timer-countdown-progress-bar")
+
 
 
 let clickCount = 0;
@@ -18,9 +20,28 @@ clickArea.addEventListener('click', () => {
 
 
 
-// TODO  when user clicks multiple buttons, multiple setIntervals run at the same time
-five.addEventListener('click', () => {
-    timer.innerHTML = parseInt(five.id);
+// TODO  when user clicks multiple buttons, multiple setIntervals run at the same time.
+// TODO add reset to clicks when seconds button is selected. functionality is already implemented in 5 seconds button
+thirty.addEventListener('click', () => {
+    clickCount = 0;
+    clickDisplay.innerHTML = clickCount
+    timer.innerHTML = parseInt(thirty.id);
+    let timerCounter = parseInt(timer.innerHTML)
+    var timerInterval = setInterval(timerCountdown, 1000)
+    function timerCountdown() {
+        if(timerCounter == 0){
+            clearInterval(timerInterval)
+        } else {
+            timerCounter--
+            timer.innerHTML = timerCounter;
+            // TODO smooth transiiton for timer countdown. and when a numbver over 100 is selected, shit breaks
+            timerCountdownProgressBar.style.width = `${timerCounter}%`
+        }
+           
+    }
+})
+sixty.addEventListener('click', () => {
+    timer.innerHTML = parseInt(sixty.id);
     let timerCounter = parseInt(timer.innerHTML)
     var timerInterval = setInterval(timerCountdown, 1000)
     function timerCountdown() {
@@ -33,8 +54,8 @@ five.addEventListener('click', () => {
            
     }
 })
-ten.addEventListener('click', () => {
-    timer.innerHTML = parseInt(ten.id);
+ninety.addEventListener('click', () => {
+    timer.innerHTML = parseInt(ninety.id);
     let timerCounter = parseInt(timer.innerHTML)
     var timerInterval = setInterval(timerCountdown, 1000)
     function timerCountdown() {
@@ -47,22 +68,8 @@ ten.addEventListener('click', () => {
            
     }
 })
-eleven.addEventListener('click', () => {
-    timer.innerHTML = parseInt(eleven.id);
-    let timerCounter = parseInt(timer.innerHTML)
-    var timerInterval = setInterval(timerCountdown, 1000)
-    function timerCountdown() {
-        if(timerCounter == 0){
-            clearInterval(timerInterval)
-        } else {
-            timerCounter--
-            timer.innerHTML = timerCounter;
-        }
-           
-    }
-})
-twelve.addEventListener('click', () => {
-    timer.innerHTML = parseInt(twelve.id);
+oneTwenty.addEventListener('click', () => {
+    timer.innerHTML = parseInt(oneTwenty.id);
     let timerCounter = parseInt(timer.innerHTML)
     var timerInterval = setInterval(timerCountdown, 1000)
     function timerCountdown() {
