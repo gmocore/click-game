7beae67c10195b457f8653e432bff5c431741f39
gerritt <gmocore@gmail.com>
2019-06-16 14:41:40 -0600

    added dynamic countdown based on user selcting button

--- main.js
+++ main.js
@@ -1,38 +1,72 @@
-const button = document.querySelector('.button');
 const clickArea = document.querySelector('.click-area');
 const timer = document.getElementById('timer');
+const five = document.getElementById('5');
+const ten = document.getElementById('10');
+const eleven = document.getElementById('11');
+const twelve = document.getElementById('12');
 
-button.addEventListener('click', () => {
-    console.log("button clicked");
-    
-})
+
 
 clickArea.addEventListener('click', () => {
     console.log("area clicked");
-    
 })
 
-let timerCounter = parseInt(timer.innerHTML)
-var timerInterval = setInterval(timerCountdown, 1000)
-function timerCountdown() {
-    if(timerCounter == 0){
-        clearInterval(timerInterval)
-    } else {
-        timerCounter--
-        console.log(timerCounter);
-        timer.innerHTML = timerCounter;
+// TODO  when user clicks multiple buttons, multiple setIntervals run at the same time
+five.addEventListener('click', () => {
+    timer.innerHTML = parseInt(five.id);
+    let timerCounter = parseInt(timer.innerHTML)
+    var timerInterval = setInterval(timerCountdown, 1000)
+    function timerCountdown() {
+        if(timerCounter == 0){
+            clearInterval(timerInterval)
+        } else {
+            timerCounter--
+            timer.innerHTML = timerCounter;
+        }
+           
     }
-       
-}
-
-
-
-
-
-
-
-
-
-
+})
+ten.addEventListener('click', () => {
+    timer.innerHTML = parseInt(ten.id);
+    let timerCounter = parseInt(timer.innerHTML)
+    var timerInterval = setInterval(timerCountdown, 1000)
+    function timerCountdown() {
+        if(timerCounter == 0){
+            clearInterval(timerInterval)
+        } else {
+            timerCounter--
+            timer.innerHTML = timerCounter;
+        }
+           
+    }
+})
+eleven.addEventListener('click', () => {
+    timer.innerHTML = parseInt(eleven.id);
+    let timerCounter = parseInt(timer.innerHTML)
+    var timerInterval = setInterval(timerCountdown, 1000)
+    function timerCountdown() {
+        if(timerCounter == 0){
+            clearInterval(timerInterval)
+        } else {
+            timerCounter--
+            timer.innerHTML = timerCounter;
+        }
+           
+    }
+})
+twelve.addEventListener('click', () => {
+    timer.innerHTML = parseInt(twelve.id);
+    let timerCounter = parseInt(timer.innerHTML)
+    var timerInterval = setInterval(timerCountdown, 1000)
+    function timerCountdown() {
+        if(timerCounter == 0){
+            clearInterval(timerInterval)
+        } else {
+            timerCounter--
+            timer.innerHTML = timerCounter;
+        }
+           
+    }
+})
 
 
