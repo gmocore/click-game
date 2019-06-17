1740b833afbb955849e5133072df083f1b501d79
Black <gmocore@gmail.com>
2019-06-17 15:46:58 -0600

    added countdown timer with dynamic seconds based on user selection

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // TODO:  When user clicks multiple buttons, multiple timers run at the same time.
 // TODO:  Need a reset button of some sort.  Since when you click on one of the seconds buttons it will start the timer and when the timer is done clicking the button doesn't do anything.
 // TODO:  innerHTML for startTimer() just sets the innerHTML but doesn't update as timer bar goes down.
+// TODO:  Update thirty’s ID to no longer be 5 seconds.  Update in main.js and index.html
 
 // Functionality Option: click listener for whole window.
 // window.addEventListener("click", () => {
@@ -10,15 +11,14 @@
 // });
 
 // Variables
-const clickArea = document.querySelector('.click-area');
-const timer = document.getElementById('timer');
-const thirty = document.getElementById('5');
-const sixty = document.getElementById('60');
-const ninety = document.getElementById('90');
-const oneTwenty = document.getElementById('120');
-const clickDisplay = document.getElementById('click-display');
+const clickArea = document.querySelector(".click-area");
+const timer = document.getElementById("timer");
+const thirty = document.getElementById("5");
+const sixty = document.getElementById("60");
+const ninety = document.getElementById("90");
+const oneTwenty = document.getElementById("120");
+const clickDisplay = document.getElementById("click-display");
 let clickCount = 0;
-
 
 // FUNCTIONS
 function clickCounterReset() {
@@ -27,38 +27,46 @@ function clickCounterReset() {
 }
 
 function startTimer(seconds) {
-    timer.style.animationDuration = `${seconds.id}s`;
-    timer.style.animationPlayState = 'running';
-    timer.innerHTML = seconds.id;
-}
+  timer.style.animationDuration = `${seconds.id}s`;
+  timer.style.animationPlayState = "running";
+  timer.innerHTML = seconds.id;
 
+  var timerInterval = setInterval(timerCountdown, 1000);
+  function timerCountdown() {
+    if (seconds.id == 0) {
+      clearInterval(timerInterval);
+    } else {
+      seconds.id--;
+      timer.innerHTML = seconds.id;
+    }
+  }
+}
 
 // EVENTLISTENERS
 clickArea.addEventListener("click", () => {
-    clickCount++;
-    clickDisplay.innerHTML = clickCount;
+  clickCount++;
+  clickDisplay.innerHTML = clickCount;
 });
 
-thirty.addEventListener('click', () => {
-    clickCounterReset();
-    startTimer(thirty);
+thirty.addEventListener("click", () => {
+  clickCounterReset();
+  startTimer(thirty);
 });
 
-sixty.addEventListener('click', () => {
-    clickCounterReset();
-    startTimer(sixty);
+sixty.addEventListener("click", () => {
+  clickCounterReset();
+  startTimer(sixty);
 });
 
-ninety.addEventListener('click', () => {
-    clickCounterReset();
-    startTimer(ninety);
+ninety.addEventListener("click", () => {
+  clickCounterReset();
+  startTimer(ninety);
 });
 
-oneTwenty.addEventListener('click', () => {
-    clickCounterReset();
-    startTimer(oneTwenty);
+oneTwenty.addEventListener("click", () => {
+  clickCounterReset();
+  startTimer(oneTwenty);
 });
-
 
 // /////////////////////////////////////////////////
 // KEEP THIS INCASE WE NEED TO REFERENCE IT LATER.
