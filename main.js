5758ecd2a35ae40de07a259f96264d729ff38703
gleekzorp <gleek.dan@gmail.com>
2019-06-22 16:22:53 -0600

    new event listener for parent div of buttons and set a new function to run based on which button was clicked from the parent div.  Still has bug with multiple button clicks

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // TODO:  Need a reset button of some sort.  Since when you click on one of the seconds buttons it will start the timer and when the timer is done clicking the button doesn't do anything.
 // TODO:  Still need some modifications to setting the timers InnerHTML.  We don't want the green bar to come back when finished.
 // TODO:  Update thirty’s ID to no longer be 5 seconds.  Update in main.js and index.html
+// TODO:  Need to make click area not clickable when timer is done.  I have a removeEventHandler in the clearInterval for now if you comment it out it will work... But I'm not sure that is the best way to do it.
 
 // Functionality Option: click listener for whole window.
 // window.addEventListener("click", () => {
@@ -13,64 +14,51 @@
 // Variables
 const clickArea = document.querySelector(".click-area");
 const timer = document.getElementById("timer");
-const thirty = document.getElementById("5");
-const sixty = document.getElementById("60");
-const ninety = document.getElementById("90");
-const oneTwenty = document.getElementById("120");
+const parentTimeSelect = document.querySelector("#time-select");
 const clickDisplay = document.getElementById("click-display");
 let clickCount = 0;
 
+// EVENTLISTENERS
+parentTimeSelect.addEventListener("click", startTimer, false);
+clickArea.addEventListener("click", clickAreaCounter, false);
+
 // FUNCTIONS
+function clickAreaCounter() {
+  clickCount++;
+  clickDisplay.innerHTML = clickCount;
+}
+
 function clickCounterReset() {
   clickCount = 0;
   clickDisplay.innerHTML = clickCount;
 }
 
-function startTimer(seconds) {
-  timer.style.animationDuration = `${seconds.id}s`;
-  timer.style.animationPlayState = "running";
-  timer.innerHTML = seconds.id;
-
-  var timerInterval = setInterval(timerCountdown, 1000);
-  function timerCountdown() {
-    if (seconds.id == 0) {
-      clearInterval(timerInterval);
-    } else {
-      seconds.id--;
-      timer.innerHTML = seconds.id;
-    }
+function startTimer(event) {
+  clickCounterReset()
+  if (event.target !== event.currentTarget) {
+    var clickedItem = event.target.id;
+    timer.style.animationDuration = `${clickedItem}s`;
+    timer.style.animationPlayState = "running";
+    timer.innerHTML = clickedItem;
+    var timerInterval = setInterval(function() {
+      clickedItem--;
+      timer.innerHTML = clickedItem;
+      if (clickedItem == 0) {
+        clearInterval(timerInterval);
+        timer.innerHTML = 'You Clicked ' + clickCount + ' Times.';
+        // clickArea.removeEventListener("click", clickAreaCounter, false);
+      }
+    }, 1000);
   }
+  event.stopPropagation()
 }
 
-// EVENTLISTENERS
-clickArea.addEventListener("click", () => {
-  clickCount++;
-  clickDisplay.innerHTML = clickCount;
-});
 
-thirty.addEventListener("click", () => {
-  clickCounterReset();
-  startTimer(thirty);
-});
 
-sixty.addEventListener("click", () => {
-  clickCounterReset();
-  startTimer(sixty);
-});
+//////////////////////////////////////////////////////
+// KEEP THIS INCASE WE NEED TO REFERENCE IT LATER. //
+////////////////////////////////////////////////////
 
-ninety.addEventListener("click", () => {
-  clickCounterReset();
-  startTimer(ninety);
-});
-
-oneTwenty.addEventListener("click", () => {
-  clickCounterReset();
-  startTimer(oneTwenty);
-});
-
-// /////////////////////////////////////////////////
-// KEEP THIS INCASE WE NEED TO REFERENCE IT LATER.
-// ////////////////////////////////////////////////
 // thirty.addEventListener('click', () => {
 //     clickCounterReset()
 //     timer.innerHTML = parseInt(thirty.id);
@@ -87,3 +75,27 @@ oneTwenty.addEventListener("click", () => {
 //     }
 //   }
 // );
+
+
+
+//////////////////////////////////////////////////
+//////             Jquery Way              //////
+////////////////////////////////////////////////
+
+// Basically comment everything else out above and I just have it running on auto pilot... I gave up as I want to just use javascript not jquery
+// You also have to add this script tag in the head <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+// https://stackoverflow.com/questions/24530908/showing-a-time-countdown-progress-bar
+// https://codepen.io/Rudchyk/pen/qNOEGj
+
+// const thirty = document.getElementById("5");
+// thirty.addEventListener('click', progress(20, 20, $('#timer')), false)
+
+// function progress(timeleft, timetotal, $element) {
+//   var progressBarWidth = timeleft * $element.width() / timetotal;
+//   $element.animate({ width: progressBarWidth }, timeleft == timetotal ? 0 : 1000, 'linear').html(timeleft + " seconds to go");
+//   if(timeleft > 0) {
+//       setTimeout(function() {
+//           progress(timeleft - 1, timetotal, $element);
+//       }, 1000);
+//   }
+// };
